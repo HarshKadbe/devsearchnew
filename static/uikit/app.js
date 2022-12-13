2ffcdc6218c830d41160a8f1f487f03ba5d9f53b
Harsh <harshkadbe085@gmail.com>
2022-12-13 19:53:44 +0530

    THIS IS OUR FIRST COMMIT

---
+++ static/uikit/app.js
@@ -0,0 +1,15 @@
+// Invoke Functions Call on Document Loaded
+document.addEventListener('DOMContentLoaded', function () {
+  hljs.highlightAll();
+});
+
+
+let alertwrapper = document.querySelector('.alert')
+let alertclose = document.querySelector('.alert__close')
+
+
+if (alertWrapper){
+  alertClose.addEventListener('click', () =>
+    alertwrapper.getElementsByClassName.display = 'none'
+  )
+}
